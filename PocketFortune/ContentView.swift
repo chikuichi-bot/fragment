de91b50caeb9e9ff58d0133f4dd7d18923a74920
Takehito Awashima <chikuichi@gmail.com>
2026-03-28 11:17:33 +0900

    Initial Commit

---
+++ PocketFortune/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketFortune
+//
+//  Created by awashima takehito on 2026/03/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketFortune/PocketFortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketFortuneApp.swift
+//  PocketFortune
+//
+//  Created by awashima takehito on 2026/03/28.
+//
+
+import SwiftUI
+
+@main
+struct PocketFortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
